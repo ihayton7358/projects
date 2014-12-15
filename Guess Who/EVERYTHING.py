d3d568a0f7169fa372f17431484e5b72880093bb
ihayton7358 <ihayton7358@sohamcollege.org.uk>
2014-12-15 12:35:08 +0000

    test

--- Guess Who/EVERYTHING.py
+++ Guess Who/EVERYTHING.py
@@ -27,41 +27,47 @@ def getUserImage(name):
     
 
 def getCharProfile():
+    CHECK = False
+    while CHECK == False:
+        name = input("What is your name")
 
-    name = input("What is your name")
+        filename = getUserImage(name)
 
-    filename = getUserImage(name)
+        gender = ""  
+        while not(gender.lower().strip() in("girl","boy")):
+            gender = input("Are you a girl or a boy?")
 
-    gender = ""  
-    while not(gender.lower().strip() in("girl","boy")):
-        gender = input("Are you a girl or a boy?")
+        hat = ""
+        while not(hat.lower().strip() in ("yes","no")):
+            hat = input("Are you wearing a hat?")
+        
+        eye_colour = ""
+        while not(eye_colour.lower().strip() in("blue","brown","green","grey","hazel")):
+                eye_colour = input("What is your eye colour?")
+            
+        hair_colour = ""
+        while not(hair_colour.lower().strip() in ("brown","black","blonda","red","ginger")):
+                hair_colour = input("What colour is your hair?")
+            
+        facial_hair = ""
+        while not(facial_hair.lower().strip() in ("yes","no")):
+            facial_hair = input("Do you have any facial hair? yes/no")
 
-    hat = ""
-    while not(hat.lower().strip() in ("yes","no")):
-        hat = input("Are you wearing a hat?")
+        if facial_hair == "yes":
+                  hair_type = ""
+                  while not(hair_type.lower().strip() in ("beard","moustache")):
+                            hair_type = input("What type of facial hair do you have?")
+
+        glasses = ""
+        while not(glasses.lower().strip() in ("yes","no")):
+            glasses = input("Do you have glasses?")
+
+        print ([name, filename, gender, hat, eye_colour, hair_colour, facial_hair, hair_type])
     
-    eye_colour = ""
-    while not(eye_colour.lower().strip() in("blue","brown","green","grey","hazel")):
-            eye_colour = input("What is your eye colour?")
-        
-    hair_colour = ""
-    while not(hair_colour.lower().strip() in ("brown","black","blonda","red","ginger")):
-            hair_colour = input("What colour is your hair?")
-        
-    facial_hair = ""
-    while not(facial_hair.lower().strip() in ("yes","no")):
-        facial_hair = input("Do you have any facial hair? Y/N")
-
-    if facial_hair == "yes":
-              hair_type = ""
-              while not(hair_type.lower().strip() in ("beard","moustache")):
-                        hair_type = input("What type of facial hair do you have?")
-
-    glasses = ""
-    while not(glasses.lower().strip() in ("yes","no")):
-        glasses = input("Do you have glasses?")
-
-    return [name, filename, gender, hat, eye_colour, hair_colour, facial_hair, hair_type]
+        check = input("Is this correct?")
+        if check.lower().strip() == "yes":
+            CHECK = True
+            
 
     
 
